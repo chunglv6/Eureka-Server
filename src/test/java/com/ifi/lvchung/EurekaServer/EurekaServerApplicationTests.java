f0be07423137e4df7c508070279b7e4830ae1669
Le Van chung <Chung.LeVan@nttdata.com>
2021-07-07 08:52:17 +0700

    first commit

---
+++ src/test/java/com/ifi/lvchung/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifi.lvchung.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
